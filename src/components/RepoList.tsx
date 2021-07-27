2225a1a221acc756e7f82264cd2add9ad287c732
Tony <tony.js@zoho.eu>
2021-07-27 10:12:00 -0300

    feat: repositories components group

--- src/components/RepoList.tsx
+++ src/components/RepoList.tsx
@@ -1,20 +1,88 @@
-import React from 'react';
-import {FlatList, View} from 'react-native';
-import {IRepo} from '../interfaces/IRepo';
+import React, {useMemo, useState} from 'react';
+import {StyleSheet, TextInput, FlatList, View, Text} from 'react-native';
+import {IRepository} from '../interfaces/IRepository';
 import {RepoListItem} from './RepoListItem';
 
 export interface RepoListProps {
-  repos: IRepo[];
+  repositories: IRepository[];
 }
 
-export const RepoList = ({repos}: RepoListProps) => {
+interface HeaderRepositoryListProps {
+  search: string;
+  setSearch: (search: string) => void;
+}
+
+export const RepoList = ({
+  repositories: receiveRepositories,
+}: RepoListProps) => {
+  const [search, setSearch] = useState('');
+
+  const repositories = useMemo(() => {
+    const regex = RegExp(search, 'i');
+    const test = (...strings: [string, ...(string | undefined | null)[]]) =>
+      strings.some(string => string && regex.test(string));
+
+    return receiveRepositories.filter(repository =>
+      test(repository.name, repository.description),
+    );
+  }, [search, receiveRepositories]);
+
   return (
     <View>
+      {/**
+       * Why not use ListHeaderComponent?
+       *
+       * Simple when the search state changes the FlatList will render the component
+       * function ListHeaderComponent again, causing it to have a small delay in the native part,
+       * so when the state changes the virtual keyboard is removed because there is a small padding
+       * in the native part.
+       *
+       * That way, removing it from within the FlatList react manages to improve the perspective natively.
+       *
+       * A way to fix this and use the ListHeaderComponent, one of the ideas would be to use passing value
+       * by reference with a submit action. However I wanted to cause a real-time filtering effect.
+       */}
+      <View style={styles.searchContainer}>
+        <TextInput
+          onChangeText={setSearch}
+          value={search}
+          style={styles.searchInput}
+          placeholder="Search for..."
+          placeholderTextColor="grey"
+        />
+      </View>
       <FlatList
-        data={repos}
+        data={repositories}
         keyExtractor={repo => `${repo.owner.id}.repos.${repo.id}`}
         renderItem={({item}) => <RepoListItem repo={item} />}
+        ListEmptyComponent={() => (
+          <View style={styles.emptyContainer}>
+            <Text style={styles.emptyText}>No public repository</Text>
+          </View>
+        )}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  emptyContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  emptyText: {
+    fontFamily: 'Roboto-Light',
+  },
+  searchContainer: {
+    backgroundColor: 'white',
+    borderBottomColor: '#DDD',
+    borderBottomWidth: 1,
+  },
+  searchInput: {
+    backgroundColor: 'white',
+    width: '100%',
+    paddingHorizontal: 10,
+    color: 'black',
+    fontFamily: 'Roboto-Light',
+  },
+});
--- src/components/RepoListItem.tsx
+++ src/components/RepoListItem.tsx
@@ -1,17 +1,109 @@
 import React, {memo} from 'react';
+import {StyleSheet, TouchableOpacity} from 'react-native';
 import {Text, View} from 'react-native';
-import {IRepo} from '../interfaces/IRepo';
+import {IRepository} from '../interfaces/IRepository';
+import moment from 'moment';
+import Octicons from 'react-native-vector-icons/Octicons';
+import shortNumber from 'short-number';
 
 export interface RepoListItemProps {
-  repo: IRepo;
+  repo: IRepository;
 }
 
 export const RepoListItemComponent = ({repo}: RepoListItemProps) => {
   return (
-    <View>
-      <Text>{repo.name}</Text>
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <View style={styles.tinyInfoContainer}>
+          <View style={styles.tinyInfoIconContainer}>
+            <Octicons name="star" style={styles.tinyInfoIcon} />
+            <Text style={styles.tinyInfoText}>
+              {shortNumber(repo.stargazers_count)}
+            </Text>
+          </View>
+          <View style={styles.tinyInfoIconContainer}>
+            <Octicons name="eye" style={styles.tinyInfoIcon} />
+            <Text style={styles.tinyInfoText}>
+              {shortNumber(repo.watchers)}
+            </Text>
+          </View>
+          <View style={styles.tinyInfoIconContainer}>
+            <Octicons name="repo-forked" style={styles.tinyInfoIcon} />
+            <Text style={styles.tinyInfoText}>
+              {shortNumber(repo.forks_count)}
+            </Text>
+          </View>
+          <View style={styles.tinyInfoIconContainer}>
+            <Octicons name="git-branch" style={styles.tinyInfoIcon} />
+            <Text style={styles.tinyInfoText}>{repo.default_branch}</Text>
+          </View>
+          <Text style={styles.lastUpdateAt}>
+            {moment(repo.updated_at).calendar()}
+          </Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.title}>{repo.name}</Text>
+        </View>
+        <View>
+          {repo.description && (
+            <Text style={styles.description}>{repo.description}</Text>
+          )}
+        </View>
+        {repo.language && (
+          <Text style={styles.repoLanguage}>{repo.language.toLowerCase()}</Text>
+        )}
+      </TouchableOpacity>
     </View>
   );
 };
 
 export const RepoListItem = memo(RepoListItemComponent);
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#EEE',
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  description: {
+    fontFamily: 'Roboto-Light',
+    marginTop: 10,
+  },
+  title: {
+    fontFamily: 'Roboto-Light',
+  },
+  lastUpdateAt: {
+    fontFamily: 'Roboto-Light',
+    fontSize: 10,
+    marginLeft: 'auto',
+  },
+  tinyInfoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  tinyInfoIconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  tinyInfoIcon: {
+    marginRight: 2,
+    fontSize: 12,
+  },
+  tinyInfoText: {
+    fontFamily: 'Roboto-Light',
+    fontSize: 12,
+  },
+  repoLanguage: {
+    fontFamily: 'Roboto-Light',
+    fontSize: 14,
+    marginTop: 10,
+  },
+});
