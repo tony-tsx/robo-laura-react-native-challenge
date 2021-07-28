b249b03746f4a8eb2fe8fcdf0e1a246481a217d3
Tony <tony.js@zoho.eu>
2021-07-28 00:51:38 -0300

    feat: first app test

--- __tests__/App.spec.tsx
+++ __tests__/App.spec.tsx
@@ -2,8 +2,42 @@ import 'react-native';
 import React from 'react';
 import App from '../src/app';
 
-import renderer from 'react-test-renderer';
+import {
+  render,
+  fireEvent,
+  waitFor,
+  act,
+  cleanup,
+} from '@testing-library/react-native';
+import {UserRepositories} from '../src/screens/UserRepositories';
+import {RepoListItemComponent} from '../src/components/RepoListItem';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+afterEach(cleanup);
+
+it('app flow using facebook nickname', async () => {
+  const rendered = render(<App />);
+
+  expect(rendered.toJSON()).toMatchSnapshot();
+
+  const textInput = rendered.getByPlaceholderText('Search nickname');
+
+  await act(async () => {
+    await fireEvent.changeText(textInput, 'facebook');
+  });
+
+  expect(rendered.toJSON()).toMatchSnapshot();
+
+  const touchableOpacity = await waitFor(() =>
+    rendered.UNSAFE_getByProps({disabled: false}),
+  );
+
+  await act(async () => {
+    await fireEvent.press(touchableOpacity);
+  });
+
+  await waitFor(() => rendered.UNSAFE_getByType(UserRepositories));
+
+  await waitFor(() => rendered.UNSAFE_getAllByType(RepoListItemComponent));
+
+  expect(rendered.toJSON()).toMatchSnapshot();
 });
