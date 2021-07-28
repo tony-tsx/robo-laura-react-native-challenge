f12a096e53f3e17bed54056a8968d3b8c263b877
Tony <tony.js@zoho.eu>
2021-07-27 21:45:05 -0300

    feat: add app icon

--- src/hooks/useOrgs.ts
+++
@@ -1,6 +0,0 @@
-import useSWR from 'swr';
-import {fetcher} from '../helpers/fetcher';
-
-export const useOrgs = (nickname: string | null) => {
-  return useSWR<any[]>(nickname ? `/users/${nickname}/repos` : null, fetcher);
-};
--- src/hooks/useRepos.ts
+++
@@ -1,7 +0,0 @@
-import useSWR from 'swr';
-import {fetcher} from '../helpers/fetcher';
-import {IRepo} from '../interfaces/IRepo';
-
-export const useRepos = (nickname: string | null) => {
-  return useSWR<IRepo[]>(nickname ? `/users/${nickname}/repos` : null, fetcher);
-};
